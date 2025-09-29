082fdbfbaca9b9ed75417dfcb36d86c96afc5d4b
IVAN-GUY <kaluginivan2002@mail.ru>
2025-09-29 19:00:41 +0300

    feat(server): Add middleware for gzip compress

--- internal/service/server.go
+++ internal/service/server.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
@@ -11,9 +12,9 @@ import (
 
 func StartServer(addr string) {
 	mux := router.New()
-	mux.GET(`/`, handler.MainPage)
-	mux.GET(`/{id}`, handler.MainPage)
-	mux.POST(`/`, handler.MainPage)
+	mux.GET(`/`, GzipMiddleware(handler.MainPage))
+	mux.GET(`/{id}`, GzipMiddleware(handler.MainPage))
+	mux.POST(`/`, GzipMiddleware(handler.MainPage))
 
 	handlerChain := fasthttp.CompressHandler(mux.Handler)
 
@@ -22,4 +23,39 @@ func StartServer(addr string) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func GzipMiddleware(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		contentEncoding := strings.ToLower(string(ctx.Request.Header.Peek("Content-Encoding")))
+		if contentEncoding == "gzip" {
+			body, err := ctx.Request.BodyGunzip()
+			if err == nil {
+				ctx.Request.SetBody(body)
+				ctx.Request.Header.Del("Content-Encoding")
+			}
+		}
+		acceptEncoding := strings.ToLower(string(ctx.Request.Header.Peek("Accept-Encoding")))
+		supportGzip := strings.Contains(acceptEncoding, "gzip")
+		next(ctx)
+		if supportGzip && ShouldCompresGzip(ctx) {
+			comressBody := fasthttp.AppendGzipBytes(nil, ctx.Response.Body())
+			ctx.Response.SetBody(comressBody)
+			ctx.Response.Header.Set("Content-Encoding", "gzip")
+		}
+	}
+}
+
+func ShouldCompresGzip(ctx *fasthttp.RequestCtx) bool {
+	contentType := strings.ToLower(string(ctx.Response.Header.Peek("Content-Type")))
+	contentTypes := []string{
+		"application/json",
+		"text/html",
+	}
+	for _, t := range contentTypes {
+		if contentType == t {
+			return true
+		}
+	}
+	return false
 }
